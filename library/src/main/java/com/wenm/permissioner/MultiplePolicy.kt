b629ebc0bd0d0aadd85c822e788676152097e912
闻谨 <wenm25177@gmail.com>
2023-05-23 22:00:10 +0800

    -initial:

---
+++ library/src/main/java/com/wenm/permissioner/MultiplePolicy.kt
@@ -0,0 +1,62 @@
+package com.qingkuang.permissioner
+
+import android.content.Intent
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
+import com.wenm.permissioner.PermissionPolicy
+import com.wenm.permissioner.checkPermissionsAllGrantedOrNot
+import com.wenm.permissioner.goToSetting
+
+class MultiplePolicy(permissions: Array<String>) : PermissionPolicy(permissions){
+
+    override fun registerLauncher(activity: FragmentActivity) {
+        super.registerLauncher(activity)
+        resultLauncher = activity.registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { result ->
+            var hasPermissionReject = false
+            val strings: Set<String> = result.keys
+
+            for (key in strings) {
+                if (!result[key]!!) {
+                    hasPermissionReject = true
+                }
+            }
+
+            if (hasPermissionReject) {
+                if (repeatCount > 0) {
+                    launch()
+                    repeatCount--
+                } else {
+                    rejectCallback?.invoke() ?: run {
+                        if (gotoSetting) {
+                            val intent: Intent = goToSetting(activity)
+                            launcher.launch(intent)
+                        } else
+                            Toast.makeText(
+                                context,
+                                "Permission is not granted, please handle the reject callback!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                    }
+                }
+            } else {
+                grantedCallback?.invoke(permissions[0], true)
+            }
+        }
+    }
+
+    override fun launch() {
+        if (checkPermissionsAllGrantedOrNot(context, permissions)) {
+            grantedCallback?.invoke(permissions[0], true)
+            return
+        }
+        resultLauncher.launch(this.permissions)
+    }
+
+    override fun launch(callback: (permission: String?, allGranted: Boolean) -> Unit) {
+        super.launch(callback)
+        launch()
+    }
+}
---
+++ library/src/main/java/com/wenm/permissioner/Permissioner.kt
@@ -0,0 +1,37 @@
+package com.wenm.permissioner
+
+import androidx.fragment.app.FragmentActivity
+import com.qingkuang.permissioner.*
+
+class Permissioner(
+    permissions: Array<String>,
+    policy: PolicyType = PolicyType.Multiple
+) : IPermissioner {
+    private val permissionPolicy: PermissionPolicy by lazy {
+        if (policy == PolicyType.Multiple) MultiplePolicy(permissions) else OneByOnePolicy(permissions)
+    }
+
+    override fun registerGrantedCallback(callback: (permission: String?, allGranted: Boolean) -> Unit) {
+        permissionPolicy.registerGrantedCallback(callback)
+    }
+
+    override fun registerRejectCallback(callback: () -> Unit) {
+        permissionPolicy.registerRejectCallback(callback)
+    }
+
+    override fun registerSettingCallback(callback: () -> Unit) {
+        permissionPolicy.registerSettingCallback(callback)
+    }
+
+    override fun registerLauncher(activity: FragmentActivity) {
+        permissionPolicy.registerLauncher(activity)
+    }
+
+    override fun launch() {
+        permissionPolicy.launch()
+    }
+
+    override fun launch(callback: (permission: String?, allGranted: Boolean) -> Unit) {
+        permissionPolicy.launch(callback)
+    }
+}
